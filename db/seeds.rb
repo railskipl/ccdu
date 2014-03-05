83e1b622f804ed702230c7eb48efb5119c09f2b9
Amol <rails@kunalinfotech.net>
2014-03-05 18:34:07 +0530

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-#puts "creating Roles"
-#Role.create(:name => "admin", :display_name => "Admin")
-# Role.create(:name => "zone", :display_name => "Zonal Level Manager")
-# Role.create(:name => "district", :display_name => "District Level Manager")
-# Role.create(:name => "block", :display_name => "Block Level Manager")
-# Role.create(:name => "mobile", :display_name => "Mobile Surveyor")
-# puts "creating admin"
+puts "creating Roles"
+Role.create(:name => "admin", :display_name => "Admin")
+Role.create(:name => "zone", :display_name => "Zonal Level Manager")
+Role.create(:name => "district", :display_name => "District Level Manager")
+Role.create(:name => "block", :display_name => "Block Level Manager")
+Role.create(:name => "mobile", :display_name => "Mobile Surveyor")
+puts "creating admin"
 admin = User.create({:email=> 'admin@example.com',:username => 'admin', :password => 'admin123!'})
 admin.roles = [Role.first]
 admin.save
